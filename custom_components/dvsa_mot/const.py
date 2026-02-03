fa361141a77c3e958231eb3543029fa1839ee7a7
Jobey99 <61569987+Jobey99@users.noreply.github.com>
2026-02-03 15:59:46 +0000

    Remove hardcoded DEFAULT_TOKEN_URL
    Removed hardcoded token URL for security reasons.

--- custom_components/dvsa_mot/const.py
+++ custom_components/dvsa_mot/const.py
@@ -18,5 +18,4 @@ CONF_SCAN_INTERVAL = "scan_interval"
 DEFAULT_WARN_DAYS = 30
 DEFAULT_SCAN_INTERVAL = 6 * 60 * 60  # seconds
 DEFAULT_SCOPE_FALLBACK = "https://tapi.dvsa.gov.uk/.default"
-DEFAULT_TOKEN_URL = "https://login.microsoftonline.com/a455b827-244f-4c97-b5b4-ce5d13b4d00c/oauth2/v2.0/token"
 DEFAULT_BASE_URL = "https://history.mot.api.gov.uk"
